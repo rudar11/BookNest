64d94893fcf1daa086e9172087c5121af5582017
rudar soni <rudar9647@gmail.com>
2026-04-17 21:46:45 +0530

    Add user signup and login functionality with flash messaging

--- app.js
+++ app.js
@@ -76,17 +76,6 @@ app.use((req, res, next) => {
 })
 
 
-app.get("/demouser", async (req, res) => {
-    let fakeUser = new User({
-        email: "student@gmail.com",
-        username: "delta-student",
-    });
-
-    let registeredUser = await User.register(fakeUser, "helloworld");
-    res.send(registeredUser);
-});
-
-
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,46 @@
 const express = require("express");
 const router = express.Router();
-
-
+const User = require("../models/users.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 router.get('/signup', (req, res) => {
     res.render("users/signup.ejs")
 })
 
+
+router.post("/signup", wrapAsync( async (req, res) => {
+try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+} catch (error) {
+    req.flash("error", error.message);
+    res.redirect("/signup");
+}
+}));
+
+
+
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to BookNest! You are logged in!");
+    res.redirect("/listings");
+  }
+);
 
 module.exports = router;
