06a8be3c4b4504f66e80c80cd12238d154d5cbbd
rudar soni <rudar9647@gmail.com>
2026-04-22 11:40:05 +0530

    Refactor listings and user routes to use route chaining; enhance show view with improved layout and review functionality; add CSS for star rating system

--- routes/listings.js
+++ routes/listings.js
@@ -2,37 +2,27 @@
 const express = require('express')
 const router = express.Router()
 const wrapAsync = require('../utils/wrapAsync.js')
-
 const Listing = require('../models/listing.js')
-
 const { isLoggedIn , isOwner , validatelisting } = require('../middleware.js')
-
 const listingController = require("../controllers/listings.js")
 
-router.get('/', wrapAsync(listingController.index))
 
+router
+.route('/')
+.get(wrapAsync(listingController.index))
+.post( isLoggedIn, validatelisting, wrapAsync(listingController.createlisting));
 
 //new route
 router.get('/new', isLoggedIn , listingController.renderNewForm)
 
-//show route
-router.get('/:id', wrapAsync(listingController.showListing))
+router.route('/:id')
+.get( wrapAsync(listingController.showListing))
+.put( isLoggedIn, isOwner, validatelisting, wrapAsync(listingController.updateListing))
+.delete( isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
-//Create Route
-router.post("/", isLoggedIn, validatelisting, wrapAsync(listingController.createlisting));
 
 //Edit Route
 router.get("/:id/edit", isLoggedIn , isOwner, wrapAsync(listingController.renderEditForm))
 
-
-
-//Update Route
-router.put("/:id", isLoggedIn, isOwner, validatelisting, wrapAsync(listingController.updateListing));
-
-
-
-
-//Delete Route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.deleteListing));
 
 module.exports = router;
